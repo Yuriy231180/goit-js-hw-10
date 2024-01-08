021a12042f4760fc0bca9ad59786f97325c81fad
Yuriy231180 <141326084+Yuriy231180@users.noreply.github.com>
2024-01-08 20:58:08 +0200

    100801.2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,32 +1,8 @@
-// Импорт IziToast
-// Прослушивание ивента на клик по кнопке
-// let timer_button = document.getElementById('timer-start');
-// timer_button.addEventListener('click', () => {
-// функция
-//   });
 
 // Описаний у документації
 import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
-
-// let delay_ms = document.getElementById();
-// // После клика вызываем функцию, которая создаёт промис
-// let promise = new Promise(function () {
-//   setTimeout(function () {
-//     // Вызов тостов
-//     // Если rejected -  error
-//     iziToast.error({
-//       title: 'Error',
-//       message: `❌ Rejected promise in ${delay_ms}ms`,
-//     });
-//     //   Если success - success
-//     iziToast.success({
-//       title: 'OK',
-//       message: `✅ Fulfilled promise in ${delay_ms}ms`,
-//     });
-//   }, delay_ms);
-// });
 
 
 const form = document.querySelector('.form');
